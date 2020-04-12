1096d96edf7ca755474b15a03cbce51074c7f3a8
shoma <shoma.h4a@gmail.com>
2020-04-12 11:29:32 +0900

    hello kotlin

---
+++ src/main/kotlin/test/web/kotlin/servlet/Test.kt
@@ -0,0 +1,15 @@
+package test.web.kotlin.servlet
+
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "hello", urlPatterns = ["/hello"])
+class Test : HttpServlet() {
+
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        resp?.contentType = "text/plain; charset=UTF-8"
+        resp?.writer?.println("hello, kotlin")
+    }
+}
